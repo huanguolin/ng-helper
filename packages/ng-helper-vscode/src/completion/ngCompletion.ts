13a3d7e26b3f2fc1d645f07fe1ab7965db8320ef
alvin huang <huanguolin@outlook.com>
2024-06-12 07:49:26 +0800

    update

--- packages/ng-helper-vscode/src/completion/ngCompletion.ts
+++ packages/ng-helper-vscode/src/completion/ngCompletion.ts
@@ -7,22 +7,48 @@ export function ngCompletion() {
         'html',
         {
             provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-                if (isComponentHtml(document) && isInStartTag(document, position)) {
-                    return getNgDirectiveList()
-                        .map(x => new vscode.CompletionItem(x));
+                if (!isComponentHtml(document)
+                    || !isInStartTagAndCanCompletion(document, position)) {
+                    return undefined;
                 }
 
-                return undefined;
+                return getNgDirectiveList()
+                    .map(x => new vscode.CompletionItem(x));
             }
         }
     );
 }
 
-function isInStartTag(document: vscode.TextDocument, position: vscode.Position) {
+function isInStartTagAndCanCompletion(document: vscode.TextDocument, position: vscode.Position) {
     const beforeText = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
-    const lastStartTag = beforeText.lastIndexOf('<');
-    const lastCloseTag = Math.max(beforeText.lastIndexOf('>'), beforeText.lastIndexOf('</'));
-    return lastCloseTag < lastStartTag;
+
+    const lastStartTagStart = beforeText.lastIndexOf('<');
+    const lastEndTagStart = beforeText.lastIndexOf('</');
+    // |
+    // |<>
+    // </|
+    if (lastStartTagStart < 0
+        || lastEndTagStart >= lastStartTagStart) {
+        return false;
+    }
+
+    // > or />
+    const lastStartTagEnd = beforeText.lastIndexOf('>');
+    // >|
+    // />|
+    if (lastStartTagEnd > lastStartTagStart) {
+        return false;
+    }
+
+    /**
+     * ><|
+     * /><|
+     * <|
+     */
+
+    // TODO can completion detect
+    const tagInnerText = beforeText.slice(lastStartTagStart);
+    return lastStartTagEnd < lastStartTagStart;
 }
 
 function getNgDirectiveList() {
