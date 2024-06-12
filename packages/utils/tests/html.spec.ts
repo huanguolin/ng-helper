5941171a0bb5493eb385d4a6c1c87fb5e81bdb72
alvin huang <huanguolin@outlook.com>
2024-06-12 22:38:52 +0800

    add more test

--- packages/utils/tests/html.spec.ts
+++ packages/utils/tests/html.spec.ts
@@ -9,6 +9,7 @@ describe('canCompletionInStartTag()', () => {
         ['<div class', false],
         ['<div class=', false],
         ['<div class="', false],
+        // ['<div class= ', false], // 这个不过也没关系
         ['<div class="btn', false],
         ['<div class="btn ', false],
         ['<div class="btn"', true],
@@ -16,6 +17,9 @@ describe('canCompletionInStartTag()', () => {
         ['<div class="btn"  tit', false],
         ['<div class="btn"  title=" ', false],
         ['<div class="btn"  title=" "', true],
+        ['<div class="btn" ng-if="click()"', true],
+        ['<div class="btn" ng-hide', false],
+        ['<div class="btn" ng-hide ', true],
     ])('input: %s => output: %s', (input: string, output: boolean) => {
         const v = canCompletionInStartTag(input);
         expect(v).toBe(output);
