4deb43903a99b7b03326149d2f92af9ea5efdd35
alvin huang <huanguolin@outlook.com>
2024-07-06 21:59:52 +0800

    feat: improve ng directive with snippet

--- packages/ng-helper-vscode/src/completion/ngCompletion.ts
+++ packages/ng-helper-vscode/src/completion/ngCompletion.ts
@@ -1,10 +1,19 @@
 import { getTemplateInnerText, isContainsNgFilter, isInTemplate } from '@ng-helper/shared/lib/html';
-import { languages, TextDocument, Position, Range, CompletionItem, CompletionList } from 'vscode';
+import { languages, TextDocument, Position, Range, CompletionItem, CompletionList, SnippetString } from 'vscode';
 
 import { getComponentControllerAs } from '../service/api';
 import { ensureTsServerRunning } from '../utils';
 
 import { isComponentHtml, isInStartTagAndCanCompletionNgX } from './utils';
+
+const defaultNgConfigExpr: NgDirectiveConfig = {
+    name: '',
+    snippet: `\${0:expression}`,
+};
+const defaultNgConfigStr: NgDirectiveConfig = {
+    name: '',
+    snippet: `\${0:string}`,
+};
 
 export function ngCompletion(port: number) {
     return languages.registerCompletionItemProvider('html', {
@@ -15,8 +24,13 @@ export function ngCompletion(port: number) {
 
             const textBeforeCursor = document.getText(new Range(new Position(0, 0), position));
             if (isInStartTagAndCanCompletionNgX(textBeforeCursor)) {
-                // TODO improve
-                return getNgDirectiveList().map((x) => new CompletionItem(x));
+                return getNgDirectiveConfigList()
+                    .map(([name, configs]) =>
+                        configs.length > 0
+                            ? configs.map((c) => configToCompletionItem(name, c))
+                            : [configToCompletionItem(name, defaultNgConfigExpr)],
+                    )
+                    .flat();
             }
 
             if (isInTemplate(textBeforeCursor)) {
@@ -41,44 +55,102 @@ async function getComponentControllerAsCompletion(document: TextDocument, port: 
     }
 }
 
-function getNgDirectiveList() {
+function configToCompletionItem(name: string, config: NgDirectiveConfig): CompletionItem {
+    const item = new CompletionItem(`${name} ${config.name}`);
+    if (config.snippet) {
+        item.insertText = new SnippetString(`${name}="${config.snippet}"`);
+    }
+    item.detail = config.description;
+    return item;
+}
+
+function getNgDirectiveConfigList(): Array<[string, NgDirectiveConfig[]]> {
     return [
-        'ng-click',
-        'ng-if',
-        'ng-model',
-        'ng-class',
-        'ng-disabled',
-        'ng-show',
-        'ng-repeat',
-        'ng-init',
-        'ng-controller',
-        'ng-options',
-        'ng-change',
-        'ng-pattern',
-        'ng-bind',
-        'ng-required',
-        'ng-maxlength',
-        'ng-hide',
-        'ng-style',
-        'ng-list',
-        'ng-dblclick',
-        'ng-submit',
-        'ng-src',
-        'ng-href',
-        'ng-checked',
-        'ng-include',
-        'ng-cloak',
-        'ng-transclude',
-        'ng-app',
-        'ng-value',
-        'ng-blur',
-        'ng-keypress',
-        'ng-selected',
-        'ng-readonly',
-        'ng-keydown',
-        'ng-form',
-        'ng-mouseover',
-        'ng-mouseleave',
-        'ng-mouseenter',
+        ['ng-click', []],
+        ['ng-if', []],
+        ['ng-model', []],
+        [
+            'ng-class',
+            [
+                {
+                    name: 'map',
+                    snippet: `{'\${1:class-name}': \${2:expression}}`,
+                },
+                {
+                    name: 'array',
+                    snippet: `['\${1:classNameVar}', '\${2:classNameVar2}']`,
+                },
+                {
+                    name: '',
+                    snippet: `\${0:classNamesVar}`,
+                },
+            ],
+        ],
+        ['ng-disabled', []],
+        ['ng-show', []],
+        [
+            'ng-repeat',
+            [
+                {
+                    name: 'array',
+                    snippet: `\${0:item} in \${1:items} track by \${2:$index}`,
+                },
+                {
+                    name: 'object',
+                    snippet: `(\${2:key}, \${0:value}) in \${1:myObject}`,
+                },
+            ],
+        ],
+        ['ng-init', []],
+        ['ng-controller', []],
+        [
+            'ng-options',
+            [
+                {
+                    name: '',
+                    snippet: `\${3:item.name} for \${2:item} in \${1:items} track by \${4:item.id}`,
+                },
+            ],
+        ],
+        ['ng-change', []],
+        [
+            'ng-pattern',
+            [
+                {
+                    name: '',
+                    snippet: `/\${0}/`,
+                },
+            ],
+        ],
+        ['ng-bind', []],
+        ['ng-required', []],
+        ['ng-maxlength', []],
+        ['ng-hide', []],
+        ['ng-style', []],
+        ['ng-dblclick', []],
+        ['ng-submit', []],
+        ['ng-src', [defaultNgConfigStr]],
+        ['ng-href', [defaultNgConfigStr]],
+        ['ng-checked', []],
+        ['ng-include', [defaultNgConfigStr]],
+        ['ng-cloak', [{ name: '' }]],
+        ['ng-transclude', [defaultNgConfigStr]],
+        ['ng-app', [{ name: '', snippet: 'angular.Module' }]],
+        ['ng-value', [defaultNgConfigStr]],
+        ['ng-blur', []],
+        ['ng-keypress', []],
+        ['ng-selected', []],
+        ['ng-readonly', []],
+        ['ng-keydown', []],
+        ['ng-form', [defaultNgConfigStr]],
+        ['ng-mouseover', []],
+        ['ng-mouseleave', []],
+        ['ng-mouseenter', []],
     ];
+}
+
+interface NgDirectiveConfig {
+    name: string;
+    description?: string;
+    snippet?: string;
 }
