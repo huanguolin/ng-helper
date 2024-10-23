63f1e64d5eea9a4e07c943aaa08eeb2d28078618
alvin huang <alvin.huang@grapecity.com>
2024-10-23 13:13:30 +0900

    complete basic cache impl

--- packages/typescript-plugin/src/ngHelperServer/cache.ts
+++ packages/typescript-plugin/src/ngHelperServer/cache.ts
@@ -1,7 +1,7 @@
 import type ts from 'typescript';
 
 import type { GetCoreContextFn, FileVersion, PluginContext } from '../type';
-import { getSourceFileVersion } from '../utils/common';
+import { getPropByName, getPropValueByName, getSourceFileVersion } from '../utils/common';
 import {
     getAngularDefineFunctionExpression,
     getAngularDefineFunctionReturnStatement,
@@ -224,119 +224,28 @@ export function buildCache(getCoreContext: GetCoreContextFn): Cache {
             if (isAngularModuleNode(ctx, node)) {
                 isNgModule = true;
             } else if (isAngularComponentRegisterNode(ctx, node)) {
-                const componentInfo = getComponentInfo(node);
+                const componentInfo = getComponentInfo(ctx, node);
                 if (componentInfo) {
                     components.push(componentInfo);
                 }
             } else if (isAngularDirectiveRegisterNode(ctx, node)) {
-                const directiveInfo = getDirectiveInfo(node);
+                const directiveInfo = getDirectiveInfo(ctx, node);
                 if (directiveInfo) {
                     directives.push(directiveInfo);
                 }
             } else if (isAngularControllerRegisterNode(ctx, node)) {
-                const controllerInfo = getControllerInfo(node);
+                const controllerInfo = getControllerInfo(ctx, node);
                 if (controllerInfo) {
                     controllers.push(controllerInfo);
                 }
             } else if (isAngularFilterRegisterNode(ctx, node)) {
-                const filterInfo = getFilterInfo(node);
+                const filterInfo = getFilterInfo(ctx, node);
                 if (filterInfo) {
                     filters.push(filterInfo);
                 }
             }
 
             ctx.ts.forEachChild(node, visitNode);
-        }
-
-        function getComponentInfo(node: ts.CallExpression): ComponentInfo | undefined {
-            // 第一个参数是字符串字面量
-            const nameNode = node.arguments[0];
-            if (ctx.ts.isStringLiteralLike(nameNode)) {
-                // TODO
-                return {
-                    name: nameNode.text,
-                    filePath: ctx.sourceFile.fileName,
-                    location: {
-                        start: nameNode.getStart(ctx.sourceFile),
-                        end: nameNode.getEnd(),
-                    },
-                    bindings: [],
-                    controllerAs: '$ctrl',
-                };
-            }
-        }
-
-        function getDirectiveInfo(node: ts.CallExpression): DirectiveInfo | undefined {
-            // 第一个参数是字符串字面量
-            const nameNode = node.arguments[0];
-            if (ctx.ts.isStringLiteralLike(nameNode)) {
-                // TODO
-                return {
-                    name: nameNode.text,
-                    filePath: ctx.sourceFile.fileName,
-                    location: {
-                        start: nameNode.getStart(ctx.sourceFile),
-                        end: nameNode.getEnd(),
-                    },
-                    restrict: 'E',
-                    scope: [],
-                };
-            }
-        }
-
-        function getControllerInfo(node: ts.CallExpression): ControllerInfo | undefined {
-            // 第一个参数是字符串字面量
-            const nameNode = node.arguments[0];
-            if (ctx.ts.isStringLiteralLike(nameNode)) {
-                return {
-                    name: nameNode.text,
-                    filePath: ctx.sourceFile.fileName,
-                    location: {
-                        start: nameNode.getStart(ctx.sourceFile),
-                        end: nameNode.getEnd(),
-                    },
-                };
-            }
-        }
-
-        function getFilterInfo(node: ts.CallExpression): FilterInfo | undefined {
-            // 第一个参数是字符串字面量
-            const nameNode = node.arguments[0];
-            if (!ctx.ts.isStringLiteralLike(nameNode)) {
-                return;
-            }
-
-            const name = nameNode.text;
-            const parameters: Parameter[] = [];
-            const funcExpr = getAngularDefineFunctionExpression(ctx, node);
-            if (funcExpr) {
-                const returnStatement = getAngularDefineFunctionReturnStatement(ctx, funcExpr);
-                if (returnStatement && returnStatement.expression && ctx.ts.isFunctionExpression(returnStatement.expression)) {
-                    for (const param of returnStatement.expression.parameters) {
-                        // 暂不考虑数组模式的可变参数和对象解构模式参数
-                        if (ctx.ts.isIdentifier(param.name)) {
-                            parameters.push({
-                                name: param.name.text,
-                                location: {
-                                    start: param.getStart(ctx.sourceFile),
-                                    end: param.getEnd(),
-                                },
-                                type: param.type ? param.type.getText(ctx.sourceFile) : 'any',
-                            });
-                        }
-                    }
-                }
-            }
-
-            return {
-                name,
-                filePath: ctx.sourceFile.fileName,
-                location: {
-                    start: nameNode.getStart(ctx.sourceFile),
-                    end: nameNode.getEnd(),
-                },
-                parameters,
-            };
         }
     }
 
@@ -371,4 +280,166 @@ export function buildCache(getCoreContext: GetCoreContextFn): Cache {
             }
         }
     }
+}
+
+function getComponentInfo(ctx: PluginContext, node: ts.CallExpression): ComponentInfo | undefined {
+    let info: ComponentInfo | undefined;
+
+    // 第一个参数是字符串字面量
+    const nameNode = node.arguments[0];
+    if (ctx.ts.isStringLiteralLike(nameNode)) {
+        info = {
+            name: nameNode.text,
+            filePath: ctx.sourceFile.fileName,
+            location: {
+                start: nameNode.getStart(ctx.sourceFile),
+                end: nameNode.getEnd(),
+            },
+            bindings: [],
+            controllerAs: '$ctrl',
+        };
+
+        // 第二个参数是对象字面量
+        const configNode = node.arguments[1];
+        if (ctx.ts.isObjectLiteralExpression(configNode)) {
+            // bindings
+            const bindingsObj = getPropByName(ctx, configNode, 'bindings');
+            if (bindingsObj && ctx.ts.isObjectLiteralExpression(bindingsObj.initializer)) {
+                info.bindings = getPropertiesOfObjLiteral(ctx, bindingsObj.initializer);
+            }
+
+            // transclude
+            const transcludeObj = getPropByName(ctx, configNode, 'transclude');
+            if (transcludeObj) {
+                info.transclude = getTranscludeInfo(ctx, transcludeObj);
+            }
+        }
+    }
+
+    return info;
+}
+
+function getDirectiveInfo(ctx: PluginContext, node: ts.CallExpression): DirectiveInfo | undefined {
+    let info: DirectiveInfo | undefined;
+
+    // 第一个参数是字符串字面量
+    const nameNode = node.arguments[0];
+    if (ctx.ts.isStringLiteralLike(nameNode)) {
+        info = {
+            name: nameNode.text,
+            filePath: ctx.sourceFile.fileName,
+            location: {
+                start: nameNode.getStart(ctx.sourceFile),
+                end: nameNode.getEnd(),
+            },
+            restrict: 'E',
+            scope: [],
+        };
+
+        // 第二个参数是对象字面量
+        const configNode = node.arguments[1];
+        if (ctx.ts.isObjectLiteralExpression(configNode)) {
+            // restrict
+            const restrictVal = getPropValueByName(ctx, configNode, 'restrict');
+            if (restrictVal && ctx.ts.isStringLiteralLike(restrictVal)) {
+                info.restrict = restrictVal.text;
+            }
+
+            // scope
+            const bindingsObj = getPropByName(ctx, configNode, 'scope');
+            if (bindingsObj && ctx.ts.isObjectLiteralExpression(bindingsObj.initializer)) {
+                info.scope = getPropertiesOfObjLiteral(ctx, bindingsObj.initializer);
+            }
+
+            // transclude
+            const transcludeObj = getPropByName(ctx, configNode, 'transclude');
+            if (transcludeObj) {
+                info.transclude = getTranscludeInfo(ctx, transcludeObj);
+            }
+        }
+    }
+
+    return info;
+}
+
+function getControllerInfo(ctx: PluginContext, node: ts.CallExpression): ControllerInfo | undefined {
+    // 第一个参数是字符串字面量
+    const nameNode = node.arguments[0];
+    if (ctx.ts.isStringLiteralLike(nameNode)) {
+        return {
+            name: nameNode.text,
+            filePath: ctx.sourceFile.fileName,
+            location: {
+                start: nameNode.getStart(ctx.sourceFile),
+                end: nameNode.getEnd(),
+            },
+        };
+    }
+}
+
+function getFilterInfo(ctx: PluginContext, node: ts.CallExpression): FilterInfo | undefined {
+    // 第一个参数是字符串字面量
+    const nameNode = node.arguments[0];
+    if (!ctx.ts.isStringLiteralLike(nameNode)) {
+        return;
+    }
+
+    const name = nameNode.text;
+    const parameters: Parameter[] = [];
+    const funcExpr = getAngularDefineFunctionExpression(ctx, node);
+    if (funcExpr) {
+        const returnStatement = getAngularDefineFunctionReturnStatement(ctx, funcExpr);
+        if (returnStatement && returnStatement.expression && ctx.ts.isFunctionExpression(returnStatement.expression)) {
+            for (const param of returnStatement.expression.parameters) {
+                // 暂不考虑数组模式的可变参数和对象解构模式参数
+                if (ctx.ts.isIdentifier(param.name)) {
+                    parameters.push({
+                        name: param.name.text,
+                        location: {
+                            start: param.getStart(ctx.sourceFile),
+                            end: param.getEnd(),
+                        },
+                        type: param.type ? param.type.getText(ctx.sourceFile) : 'any',
+                    });
+                }
+            }
+        }
+    }
+
+    return {
+        name,
+        filePath: ctx.sourceFile.fileName,
+        location: {
+            start: nameNode.getStart(ctx.sourceFile),
+            end: nameNode.getEnd(),
+        },
+        parameters,
+    };
+}
+
+function getTranscludeInfo(ctx: PluginContext, transclude: ts.ObjectLiteralElementLike): boolean | Property[] | undefined {
+    if (ctx.ts.isPropertyAssignment(transclude)) {
+        if (transclude.initializer.kind === ctx.ts.SyntaxKind.TrueKeyword) {
+            return true;
+        } else if (ctx.ts.isObjectLiteralExpression(transclude.initializer)) {
+            return getPropertiesOfObjLiteral(ctx, transclude.initializer);
+        }
+    }
+}
+
+function getPropertiesOfObjLiteral(ctx: PluginContext, objLiteral: ts.ObjectLiteralExpression): Property[] {
+    const props: Property[] = [];
+    for (const p of objLiteral.properties) {
+        if (ctx.ts.isPropertyAssignment(p) && ctx.ts.isIdentifier(p.name) && ctx.ts.isStringLiteralLike(p.initializer)) {
+            props.push({
+                name: p.name.text,
+                value: p.initializer.text,
+                location: {
+                    start: p.getStart(ctx.sourceFile),
+                    end: p.getEnd(),
+                },
+            });
+        }
+    }
+    return props;
 }
