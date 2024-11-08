e4dfca623e89f910490c2ae3d537261dcc36f1f5
alvin huang <alvin.huang@grapecity.com>
2024-11-08 17:46:02 +0900

    [ng-parser] add test coverage

--- packages/ng-parser/jest.config.js
+++ packages/ng-parser/jest.config.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
     moduleFileExtensions: ['ts', 'js'],
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['html'],
 };
