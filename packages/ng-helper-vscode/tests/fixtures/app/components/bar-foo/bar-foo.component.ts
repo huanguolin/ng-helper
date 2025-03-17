dae5d15dbad369dc7766218520edec40278d5ffa
alvin huang <huanguolin@outlook.com>
2025-03-17 21:20:04 +0800

    Add e2e test for ng-repeat:hover

--- packages/ng-helper-vscode/tests/fixtures/app/components/bar-foo/bar-foo.component.ts
+++ packages/ng-helper-vscode/tests/fixtures/app/components/bar-foo/bar-foo.component.ts
@@ -12,6 +12,15 @@ namespace app.components {
         bar!: number;
         foo!: string;
         disabled?: boolean;
+
+        obj = {
+            txt: 'txt string',
+            arr: [
+                { id: 1, name: 'item-1' },
+                { id: 3, name: 'item-2' },
+                { id: 5, name: 'item-3' },
+            ],
+        };
     }
 
     angular.module('app.components').component('barFoo', {
